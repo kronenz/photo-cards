8e624e9ccba92b32bc4fe4e8da0ad60d40b57691
kronenz <dron199939@gmail.com>
2024-07-25 14:28:36 +0000

    config fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
 import { sveltekit } from '@sveltejs/kit/vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
